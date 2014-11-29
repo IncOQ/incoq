1c4ca08c169b1653c857853ba4519f42493064b0
Jon Brandvein <jon.brandvein@gmail.com>
2014-11-29 18:35:04 -0500

    Updated env script.

--- env.sh
+++ env.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
-# Adjust PYTHONPATH to include InvInc and dependencies.
-# Assumes we're being run from the root of the project tree.
-# For Cygwin environments, invoke as `env.sh true` to use
-# windows-style paths and separators, which is necessary
-# when using a native windows (non-Cygwin) Python.
+# Adjust PYTHONPATH in the current shell to include InvInc and dependencies.
+# Invoke as `source env.sh` or `source env.sh -w`.
+#
+# The -w flag is for Cygwin environments which use a non-Cygwin
+# (native Windows) Python interpreter. It causes the path entries
+# to be added in Windows format.
 
-export PYTHONPATH="$PWD/invinc:$PWD/simplestruct:$PWD/iast:$PWD/frexp:$PWD/gendb:$PWD/distalgo"
 
-if ${1-false} ; then
-    export PYTHONPATH=`cygpath -wp $PYTHONPATH`
+getopts w WINPATH
+# Since we're invoked as a source script, clean up after ourselves
+# or we can't run twice.
+OPTIND=1
+OPTARG=
+
+# Courtesy http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+
+NEWENTRIES="$DIR:$DIR/simplestruct:$DIR/iast:$DIR/frexp:$DIR/gendb:$DIR/distalgo"
+
+if [ $WINPATH = "w" ]; then
+    NEWENTRIES=`cygpath -wp $NEWENTRIES`
+    SEP=';'
+else
+    SEP=':'
 fi
+
+if [ -z $PYTHONPATH ]; then
+    SEP=''
+fi
+
+export PYTHONPATH="${PYTHONPATH}${SEP}${NEWENTRIES}"
+echo "Updated PYTHONPATH."
