5c2b174a0796c5a7143acc82db83b8f2b77239db
Jon Brandvein <jon.brandvein@gmail.com>
2015-05-07 11:06:20 -0400

    Updated more invinc->incoq references, updated distalgo ref.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-InvInc is a system for generating incremental and demand-driven
+IncOQ is a system for generating incremental and demand-driven
 implementations of object-set queries. The current source tree
 is a development version.
 
 ## Configuration
 
-InvInc requires Python 3.4.
+IncOQ requires Python 3.4.
 
 To clone, run
 
-    git clone https://github.com/InvInc/invinc.git
+    git clone https://github.com/IncOQ/incoq.git
 
 cd to the project directory, and run
 
@@ -39,4 +39,4 @@ In addition, the following 3rd-party Python libraries are used:
  
  A single input file may be transformed by running
  
-     python34 -m invinc <input file> <output file>
+     python34 -m incoq <input file> <output file>
--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Adjust PYTHONPATH in the current shell to include InvInc and dependencies.
+# Adjust PYTHONPATH in the current shell to include IncOQ and dependencies.
 # Invoke as `source env.sh` or `source env.sh -w`.
 #
 # The -w flag is for Cygwin environments which use a non-Cygwin
@@ -15,10 +15,10 @@ OPTIND=1
 OPTARG=
 
 # Courtesy http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
-INVINC_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-DEPS_DIR=`dirname $INVINC_DIR`/deps
+INCOQ_DIR=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
+DEPS_DIR=`dirname $INCOQ_DIR`/deps
 
-NEWENTRIES="$INVINC_DIR:$DEPS_DIR/simplestruct:$DEPS_DIR/iast:$DEPS_DIR/frexp:$DEPS_DIR/distalgo:$DEPS_DIR/gendb:$DEPS_DIR/osq"
+NEWENTRIES="$INCOQ_DIR:$DEPS_DIR/simplestruct:$DEPS_DIR/iast:$DEPS_DIR/frexp:$DEPS_DIR/distalgo:$DEPS_DIR/gendb:$DEPS_DIR/osq"
 
 if [ $WINPATH = "w" ]; then
     NEWENTRIES=`cygpath -wp $NEWENTRIES`
