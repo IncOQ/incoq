12cbb7382d897867b84144e4bf7a338822264fa7
Jon Brandvein <jon.brandvein@gmail.com>
2015-08-19 11:00:06 -0400

    Don't truncate output file if transformation fails

--- incoq/mars/__main__.py
+++ incoq/mars/__main__.py
@@ -10,7 +10,7 @@ __all__ = [
 import sys
 import argparse
 
-from incoq.mars.transform import transform_file
+from incoq.mars.transform import transform_source
 
 
 def invoke(in_filename, out_filename):
@@ -21,16 +21,22 @@ def invoke(in_filename, out_filename):
     out_file = None
     
     try:
+        # Use transform_source() rather than transform_file,
+        # so we only open (and thereby truncate) the output
+        # file if transformation was successful.
         if in_filename == '-':
             in_file = sys.stdin
         else:
             in_file = open(in_filename, 'rt')
+        source = in_file.read()
+        
+        source = transform_source(source)
+        
         if out_filename == '-':
             out_file = sys.stdout
         else:
             out_file = open(out_filename, 'wt')
-        
-        transform_file(in_file, out_file)
+        out_file.write(source)
     
     finally:
         if in_file is not None and in_filename != '-':
