231c119411b7b4d546a6f2762aa2d0a5a3e9619f
Jon Brandvein <jon.brandvein@gmail.com>
2015-10-16 14:07:07 -0400

    Add QueryMarker rewriting

--- incoq/mars/transform/incast_rewritings.py
+++ incoq/mars/transform/incast_rewritings.py
@@ -5,6 +5,9 @@ __all__ = [
     # Preprocessings are paired with their postprocessings,
     # and listed in order of their application, outermost-first.
     
+    'QueryMarker',
+    'mark_queries',
+    
     'SetMapImporter',
     'SetMapExporter',
     
@@ -18,6 +21,95 @@ __all__ = [
 
 
 from incoq.mars.incast import L
+
+
+class QueryMarker(L.NodeTransformer):
+    
+    """Given a tree and a mapping from query expressions to query names,
+    mark all occurrences of the query expressions in the tree, by
+    wrapping them in a Query node. Expressions that are already the
+    direct descendant of a Query node are not marked, but their
+    subexpressions can be.
+    
+    In the case where some of the the given query expressions may
+    contain others, these occurrences of inner query expressions must
+    already be marked. For example, if the given expressions in the
+    mapping are
+    
+        A(... B ...)
+        B
+    
+    Then the occurrence of B in A must be marked, as in
+    
+        A(... QUERY(<name>, B) ...)
+        B
+    
+    This is to ensure that the rule for A matches occurrences of its
+    expression in the tree, after the subtrees of such occurrences
+    have already been processed.
+    
+    If strict is True, raise ProgramError if any given query expression
+    has no occurrence that can be marked.
+    """
+    
+    def __init__(self, query_name_map, *, strict=False):
+        super().__init__()
+        self.query_name_map = query_name_map
+        """Map from query AST to name."""
+        self.strict = strict
+        """Raise error if a query does not occur, or only occurs
+        directly inside a Query node.
+        """
+    
+    def process(self, tree):
+        self.found = set()
+        tree = super().process(tree)
+        notfound = set(self.query_name_map.keys()) - self.found
+        if self.strict and len(notfound) > 0:
+            qstrs = [L.Parser.ts(query) for query in notfound]
+            raise L.ProgramError('No matching occurrence for queries: {}'
+                                 .format(', '.join(qstrs)))
+        return tree
+    
+    def generic_visit(self, node):
+        node = super().generic_visit(node)
+        
+        # Wrap in Query if it's one of the queries we're given.
+        name = self.query_name_map.get(node, None)
+        if name is not None:
+            self.found.add(node)
+            node = L.Query(name, node)
+        
+        return node
+    
+    def visit_Query(self, node):
+        # Skip immediate child but recurse over its children.
+        # This is one of the few cases where it's appropriate
+        # for a visitor to call super().generic_visit().
+        child = super().generic_visit(node.query)
+        if child is not node.query:
+            node = node._replace(query=child)
+        return node
+
+
+def mark_queries(tree, query_name_map):
+    """Apply QueryMarker, taking care of the case where some of the
+    given queries can be nested inside other ones.
+    """
+    # Form a new query name map where all proper subquery expressions
+    # are marked. Do this by running QueryMarker on each query
+    # expression in a topologically sorted order, with the substitutions
+    # specified by the smaller expressions that have already been
+    # processed. The topological order is obtained by sorting on tree
+    # size.
+    query_name_items = list(query_name_map.items())
+    query_name_items.sort(key=L.tree_size)
+    new_map = {}
+    for query, name in query_name_items:
+        new_query = QueryMarker.run(query, new_map)
+        new_map[new_query] = name
+    
+    return QueryMarker.run(tree, new_map, strict=True)
 
 
 class SetMapImporter(L.NodeTransformer):
--- incoq/tests/mars/transform/test_incast_rewritings.py
+++ incoq/tests/mars/transform/test_incast_rewritings.py
@@ -8,6 +8,53 @@ from incoq.mars.symtab import N
 from incoq.mars.transform.incast_rewritings import *
 
 
+class QueryMarkerCase(unittest.TestCase):
+    
+    def test_basic(self):
+        tree = L.Parser.p('''
+            def main():
+                print(1 + 2)
+                print(QUERY('A', 3 + 4))
+                print(3 + 4)
+            ''')
+        query_name_map = {L.Parser.pe('1 + 2'): 'Q1',
+                          L.Parser.pe('3 + 4'): 'Q2'}
+        tree = QueryMarker.run(tree, query_name_map)
+        exp_tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q1', 1 + 2))
+                print(QUERY('A', 3 + 4))
+                print(QUERY('Q2', 3 + 4))
+            ''')
+        self.assertEqual(tree, exp_tree)
+    
+    def test_notfound(self):
+        tree = L.Parser.p('''
+            def main():
+                print(QUERY('A', 3 + 4))
+            ''')
+        with self.assertRaises(L.ProgramError):
+            QueryMarker.run(tree, {L.Parser.pe('1 + 2'): 'Q'},
+                                strict=True)
+        with self.assertRaises(L.ProgramError):
+            QueryMarker.run(tree, {L.Parser.pe('3 + 4'): 'Q'},
+                                strict=True)
+    
+    def test_nested(self):
+        tree = L.Parser.p('''
+            def main():
+                print(1 + (2 + 3))
+            ''')
+        query_name_map = {L.Parser.pe('(2 + 3)'): 'Q1',
+                          L.Parser.pe('1 + (2 + 3)'): 'Q2'}
+        tree = mark_queries(tree, query_name_map)
+        exp_tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q2', (1 + QUERY('Q1', (2 + 3)))))
+            ''')
+        self.assertEqual(tree, exp_tree)
+
+
 class SetMapCase(unittest.TestCase):
     
     def test_preprocess(self):
