1b8f06bc66cfe0bb37921223f5d6532b978fa80c
Jon Brandvein <jon.brandvein@gmail.com>
2015-12-17 20:39:00 -0500

    Move JoinExpander to new comp/trans.py file

---
+++ incoq/mars/comp/trans.py
@@ -0,0 +1,56 @@
+"""Transformation to implement comprehensions."""
+
+
+__all__ = [
+    'JoinExpander',
+]
+
+
+from incoq.mars.incast import L
+
+
+class JoinExpander(L.NodeTransformer):
+    
+    """Rewrites DecompFor loops over joins by expanding the join into
+    its nested clauses.
+    
+    A DecompFor loop is rewritable if the right-hand side is a Query
+    node wrapping a Comp node that is a join, and if the target of
+    the loop consists of the same variables (in the same order) as
+    those returned by the join.
+    
+    A rewritable DecompFor loop is rewritten if the query name matches
+    one of the given names. Other occurrences of the query are ignored.
+    """
+    
+    def __init__(self, clausetools, queries, query_params):
+        super().__init__()
+        self.clausetools = clausetools
+        """ClauseTools instance."""
+        self.queries = queries
+        """Names of queries that wrap joins that are to be expanded.
+        If None, expand all rewritable joins.
+        """
+        self.query_params = query_params
+        """Mapping from query name to tuple of parameters."""
+    
+    def visit_DecompFor(self, node):
+        ct = self.clausetools
+        
+        node = super().generic_visit(node)
+        
+        # Skip if the RHS isn't a comprehension query for which
+        # expansion was requested.
+        if not (isinstance(node.iter, L.Query) and
+                node.iter.name in self.queries and
+                isinstance(node.iter.query, L.Comp)):
+            return node
+        comp = node.iter.query
+        
+        # Check that it's a join and the variables match the loop target.
+        if not (ct.is_join(comp) and
+                ct.lhs_vars_from_comp(comp) == node.vars):
+            return node
+        
+        params = self.query_params[node.iter.name]
+        return ct.get_code_for_clauses(comp.clauses, params, node.body)
--- incoq/tests/mars/comp/test_join.py
+++ incoq/tests/mars/comp/test_join.py
@@ -96,37 +96,6 @@ class JoinCase(unittest.TestCase):
                               for (y, z) in SING(e)}'''),
         ]
         self.assertSequenceEqual(joins, exp_joins)
-    
-    def test_join_expander(self):
-        Q1 = L.Parser.pe('''{(x, y, z) for (x, y) in REL(R)
-                                       for (y, z) in REL(S)}''')
-        Q2 = L.Parser.pe('{(x,) for (x,) in REL(R)}')
-        Q3 = L.Parser.pe('{True for (x,) in REL(R)}')
-        query_params = {'Q1': ('x',), 'Q2': (), 'Q3': ()}
-        
-        tree = L.Parser.p('''
-            def main():
-                for (x, y, z) in QUERY('Q1', _Q1):
-                    pass
-                for z in QUERY('Q2', _Q2):
-                    pass
-                for z in QUERY('Q3', _Q3):
-                    pass
-            ''', subst={'_Q1': Q1, '_Q2': Q2, '_Q3': Q3})
-        
-        tree = JoinExpander.run(tree, self.ct, ['Q1', 'Q2', 'Q3'],
-                                query_params)
-        exp_tree = L.Parser.p('''
-            def main():
-                for (y,) in R.imglookup('bu', (x,)):
-                    for (z,) in S.imglookup('bu', (y,)):
-                        pass
-                for z in QUERY('Q2', {(x,) for (x,) in REL(R)}):
-                    pass
-                for z in QUERY('Q3', {True for (x,) in REL(R)}):
-                    pass
-            ''')
-        self.assertEqual(tree, exp_tree)
 
 
 if __name__ == '__main__':
