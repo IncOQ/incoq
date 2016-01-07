ff82cf55cb57d3841916399de2eb75f1ae5009f9
Jon Brandvein <jon.brandvein@gmail.com>
2016-01-07 17:23:55 -0500

    Add clause order heuristic

---
+++ incoq/mars/comp/order.py
@@ -0,0 +1,61 @@
+"""Clause ordering heuristic."""
+
+
+__all__ = [
+    'order_clauses',
+]
+
+
+from incoq.util.planner import State, Planner
+from incoq.mars.incast import L
+
+
+class OrderState(State):
+    
+    def __init__(self, clausevisitor, bindenv, chosen, remaining):
+        self.clv = clausevisitor
+        """Clause visitor for determining priorities and LHS vars."""
+        self.bindenv = bindenv
+        """Set of bound variables."""
+        self.chosen = chosen
+        """List of determined clauses, in order."""
+        self.remaining = remaining
+        """List of clauses remaining to be ordered."""
+    
+    def get_successors(self):
+        # Sort by priority under the current binding environment.
+        def keyfunc(cl):
+            return self.clv.get_priority(cl, self.bindenv)
+        def allowed(cl):
+            return keyfunc(cl) is not None
+        clauses = [cl for cl in self.remaining if allowed(cl)]
+        clauses = sorted(clauses, key=keyfunc)
+        
+        # For each, produce a successor state picking that clause,
+        # with an updated binding environment.
+        succ = []
+        for cl in clauses:
+            new_bindenv = self.bindenv | set(self.clv.lhs_vars(cl))
+            new_chosen = self.chosen + [cl]
+            new_remaining = set(self.remaining) - {cl}
+            state = OrderState(self.clv, new_bindenv,
+                               new_chosen, new_remaining)
+            succ.append(state)
+        
+        return succ
+    
+    def final(self):
+        return len(self.remaining) == 0
+    
+    def get_answer(self):
+        return self.chosen
+
+
+def order_clauses(clausevisitor, clauses):
+    """Order clauses according to their reported priorities.
+    
+    Use a greedy heuristic: Choose the leftmost clause whose priority
+    is best (lowest number). Return the ordered clauses as a list.
+    """
+    init = OrderState(clausevisitor, set(), [], clauses)
+    return Planner().get_first_answer(init)
---
+++ incoq/tests/mars/comp/test_order.py
@@ -0,0 +1,26 @@
+"""Unit tests for order.py."""
+
+
+import unittest
+
+from incoq.mars.incast import L
+from incoq.mars.comp.clause import CoreClauseVisitor
+from incoq.mars.comp.order import *
+
+
+class OrderCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.ct = CoreClauseVisitor()
+    
+    def test_order_clauses(self):
+        comp = L.Parser.pe('''{None for (a, b) in REL(R) for (a, c) in REL(R)
+                                    for (c,) in SING(e) if c > 5}''')
+        clauses = comp.clauses
+        exp_clauses = [clauses[i] for i in [2, 3, 1, 0]]
+        clauses = order_clauses(self.ct, clauses)
+        self.assertEqual(clauses, exp_clauses)
+
+
+if __name__ == '__main__':
+    unittest.main()
