43f95fdfdf5c90515d934e4279d8dec8213d1908
Jon Brandvein <jon.brandvein@gmail.com>
2016-01-31 20:17:54 -0500

    Add rest of basic aggr incrementalization logic

--- incoq/mars/aggr/trans.py
+++ incoq/mars/aggr/trans.py
@@ -3,13 +3,16 @@
 
 __all__ = [
     'AggrInvariant',
+    'AggrMaintainer',
+    'incrementalize_aggr',
 ]
 
 
 from simplestruct import Struct, TypedField
 
 from incoq.mars.incast import L
-from incoq.mars.symbol import N
+from incoq.mars.type import T
+from incoq.mars.symbol import S, N
 
 from .aggrop import handler_for_op
 
@@ -39,6 +42,9 @@ class AggrInvariant(Struct):
     def get_maint_func_name(self, op):
         op_name = L.set_update_name(op)
         return N.get_maint_func_name(self.map, self.rel, op_name)
+    
+    def get_handler(self):
+        return handler_for_op[self.op.__class__]()
 
 
 def make_aggr_maint_func(fresh_vars, aggrinv, op):
@@ -60,7 +66,7 @@ def make_aggr_maint_func(fresh_vars, aggrinv, op):
     state = fresh_var_prefix + '_state'
     
     # Logic specific to aggregate operator.
-    handler = handler_for_op[aggrinv.op.__class__]()
+    handler = aggrinv.get_handler()
     zero = handler.make_zero_expr()
     empty = handler.make_empty_cond(state)
     updatestate_code = handler.make_update_state_code(fresh_var_prefix,
@@ -113,3 +119,120 @@ def make_aggr_maint_func(fresh_vars, aggrinv, op):
                     '<c>_SETSTATE': setstate_code})
     
     return func
+
+
+class AggrMaintainer(L.NodeTransformer):
+    
+    def __init__(self, fresh_vars, aggrinv):
+        super().__init__()
+        self.fresh_vars = fresh_vars
+        self.aggrinv = aggrinv
+    
+    def visit_Module(self, node):
+        node = self.generic_visit(node)
+        
+        funcs = []
+        for op in [L.SetAdd(), L.SetRemove()]:
+            func = make_aggr_maint_func(self.fresh_vars, self.aggrinv, op)
+            funcs.append(func)
+        
+        node = node._replace(decls=tuple(funcs) + node.decls)
+        return node
+    
+    def visit_RelUpdate(self, node):
+        if not isinstance(node.op, (L.SetAdd, L.SetRemove)):
+            return node
+        if node.rel != self.aggrinv.rel:
+            return node
+        
+        op_name = L.set_update_name(node.op)
+        func_name = N.get_maint_func_name(self.aggrinv.map,
+                                          node.rel, op_name)
+        
+        code = (node,)
+        call_code = (L.Expr(L.Call(func_name, [L.Name(node.elem)])),)
+        code = L.insert_rel_maint(code, call_code, node.op)
+        return code
+    
+    def visit_RelClear(self, node):
+        # TODO
+        assert()
+
+
+def get_rel_type(symtab, rel):
+    """Helper for returning a relation's element type."""
+    # This helper is used below, but it should probably be refactored
+    # into a general helper in the type subpackage.
+    relsym = symtab.get_symbols().get(rel, None)
+    if relsym is None:
+        raise L.TransformationError('No symbol info for operand relation {}'
+                                    .format(rel))
+    t_rel = relsym.type
+    # Bad form: What if we have a Set<Bottom>?
+    if not (isinstance(t_rel, T.Set) and
+            isinstance(t_rel.elt, T.Tuple)):
+        raise L.ProgramError('Bad type for relation {}: {}'.format(
+                             rel, t_rel))
+    return t_rel
+
+
+def aggrinv_from_query(symtab, query, result_var):
+    """Determine the aggregate invariant info for a given query."""
+    node = query.node
+    assert isinstance(node, L.Aggr)
+    
+    # Get rel, mask, and param info.
+    if isinstance(node.value, L.Name):
+        rel = node.value.id
+        # Mask will be all-unbound, filled in below.
+        mask = None
+        params = ()
+    elif (isinstance(node.value, L.ImgLookup) and
+          isinstance(node.value.set, L.Name)):
+        rel = node.value.set.id
+        mask = node.value.mask
+        params = node.value.bounds
+    else:
+        raise L.ProgramError('Unknown aggregate form: {}'.format(node))
+    
+    # Lookup symbol, use type info to determine the relation's arity.
+    t_rel = get_rel_type(symtab, rel)
+    arity = len(t_rel.elt.elts)
+    
+    if mask is None:
+        mask = L.mask('u' * arity)
+    else:
+        # Confirm that this arity is consistent with the above mask.
+        assert len(mask.m) == arity
+    
+    return AggrInvariant(result_var, node.op, rel, mask, params)
+
+
+def incrementalize_aggr(tree, symtab, query, result_var):
+    # Form the invariant.
+    aggrinv = aggrinv_from_query(symtab, query, result_var)
+    handler = aggrinv.get_handler()
+    
+    # Transform to maintain it.
+    tree = AggrMaintainer.run(tree, symtab.fresh_names.vars, aggrinv)
+    
+    # Transform occurrences of the aggregate.
+    
+    class CompExpander(S.QueryRewriter):
+        def rewrite(self, symbol, name, expr):
+            if name == query.name:
+                return L.DictLookup(L.Name(aggrinv.map),
+                                    L.tuplify(aggrinv.params),
+                                    handler.make_zero_expr())
+    
+    tree = CompExpander.run(tree, symtab, expand=True)
+    
+    # Determine the result map's type and define its symbol.
+    t_rel = get_rel_type(symtab, aggrinv.rel)
+    btypes, _ = L.split_by_mask(aggrinv.mask, t_rel.elt.elts)
+    t_key = T.Tuple(btypes)
+    t_val = handler.result_type(t_rel)
+    t_map = T.Map(t_key, t_val)
+    symtab.define_map(aggrinv.map, type=t_map)
+    
+    return tree
--- incoq/tests/mars/aggr/test_trans.py
+++ incoq/tests/mars/aggr/test_trans.py
@@ -4,9 +4,10 @@
 import unittest
 
 from incoq.mars.incast import L
-from incoq.mars.symbol import N
+from incoq.mars.type import T
+from incoq.mars.symbol import S, N
 from incoq.mars.aggr.trans import *
-from incoq.mars.aggr.trans import make_aggr_maint_func
+from incoq.mars.aggr.trans import make_aggr_maint_func, aggrinv_from_query
 
 
 class TransCase(unittest.TestCase):
@@ -44,6 +45,71 @@ class TransCase(unittest.TestCase):
                     A.mapassign(_v1_key, _v1_state)
             ''')
         self.assertEqual(func, exp_func)
+    
+    def test_aggrmaintainer(self):
+        aggrinv = AggrInvariant('A', L.Count(), 'R',
+                                L.mask('u'), ())
+        tree = L.Parser.p('''
+            def main():
+                R.reladd(x)
+                R.relremove(x)
+            ''')
+        tree = AggrMaintainer.run(tree, N.fresh_name_generator(), aggrinv)
+        self.assertEqual(tree.decls[0].name, '_maint_A_for_R_add')
+        self.assertEqual(tree.decls[1].name, '_maint_A_for_R_remove')
+        main = tree.decls[2]
+        exp_main = L.Parser.ps('''
+            def main():
+                R.reladd(x)
+                _maint_A_for_R_add(x)
+                _maint_A_for_R_remove(x)
+                R.relremove(x)
+            ''')
+        self.assertEqual(main, exp_main)
+    
+    def test_aggrinv_from_query(self):
+        # Relation operand.
+        symtab = S.SymbolTable()
+        aggr = L.Parser.pe('count(R)')
+        symtab.define_relation('R', type=T.Set(T.Tuple([T.Number, T.Number])))
+        query = symtab.define_query('Q', node=aggr)
+        aggrinv = aggrinv_from_query(symtab, query, 'A')
+        exp_aggrinv = AggrInvariant('A', L.Count(), 'R',
+                                    L.mask('uu'), ())
+        self.assertEqual(aggrinv, exp_aggrinv)
+        
+        # ImgLookup operand.
+        symtab = S.SymbolTable()
+        aggr = L.Parser.pe("count(R.imglookup('bu', (x,)))")
+        symtab.define_relation('R', type=T.Set(T.Tuple([T.Number, T.Number])))
+        query = symtab.define_query('Q', node=aggr)
+        aggrinv = aggrinv_from_query(symtab, query, 'A')
+        exp_aggrinv = AggrInvariant('A', L.Count(), 'R',
+                                    L.mask('bu'), ('x',))
+        self.assertEqual(aggrinv, exp_aggrinv)
+    
+    def test_incrementalize_aggr(self):
+        symtab = S.SymbolTable()
+        aggr = L.Parser.pe("count(R.imglookup('bu', (x,)))")
+        symtab.define_relation('R', type=T.Set(T.Tuple([T.Number, T.Number])))
+        query = symtab.define_query('Q', node=aggr)
+        tree = L.Parser.p('''
+            def main():
+                R.add(x)
+                print(QUERY('Q', count(R.imglookup('bu', (x,)))))
+            ''')
+        
+        tree = incrementalize_aggr(tree, symtab, query, 'A')
+        main = tree.decls[2]
+        exp_main = L.Parser.ps('''
+            def main():
+                R.add(x)
+                print(A.get((x,), 0))
+            ''')
+        self.assertEqual(main, exp_main)
+        
+        sym = symtab.get_maps()['A']
+        self.assertEqual(sym.type, T.Map(T.Tuple([T.Number]), T.Number))
 
 
 if __name__ == '__main__':
