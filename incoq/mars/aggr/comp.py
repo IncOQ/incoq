3ba3658a2fa2996a5451704219e4b54d495546b9
Jon Brandvein <jon.brandvein@gmail.com>
2016-02-09 13:01:12 -0500

    Add aggr/comp.py for rewriting comps with nested aggregates

---
+++ incoq/mars/aggr/comp.py
@@ -0,0 +1,100 @@
+"""Rewriting for aggregates in comprehensions."""
+
+
+__all__ = [
+    'AggrMapReplacer',
+    'AggrMapCompRewriter',
+    'transform_comps_with_maps',
+]
+
+
+from collections import OrderedDict
+
+from incoq.util.collections import OrderedSet
+from incoq.mars.incast import L
+from incoq.mars.symbol import S, N
+from incoq.mars.auxmap import SetFromMapInvariant, transform_setfrommap
+
+
+class AggrMapReplacer(L.NodeTransformer):
+    
+    """Replace all occurrences of map lookups with fresh variables.
+    Return the transformed AST, a list of new clauses binding the
+    fresh variables, and a set of SetFromMap invariants that need
+    to be transformed.
+    
+    This is intended to be used on each of a comprehension's clauses and
+    its result expression, reusing the same transformer instance.
+    """
+    
+    def __init__(self, fresh_names):
+        super().__init__()
+        self.fresh_names = fresh_names
+        self.repls = OrderedDict()
+        """Mapping from nodes to replacement variables."""
+        self.sfm_invs = OrderedSet()
+        """SetFromMap invariants."""
+    
+    def process(self, tree):
+        self.new_clauses = []
+        tree = super().process(tree)
+        return tree, self.new_clauses
+    
+    def visit_DictLookup(self, node):
+        node = self.generic_visit(node)
+        
+        # Only simple map lookups are allowed.
+        assert isinstance(node.value, L.Name)
+        assert L.is_tuple_of_names(node.key)
+        assert node.default is None
+        map = node.value.id
+        keyvars = L.detuplify(node.key)
+        
+        var = self.repls.get(node, None)
+        if var is None:
+            mask = L.mapmask_from_len(len(keyvars))
+            
+            # Create a fresh variable.
+            self.repls[node] = var = next(self.fresh_names)
+            
+            # Construct a clause to bind it.
+            vars = list(keyvars) + [var]
+            new_clause = L.SetFromMapMember(vars, map, mask)
+            self.new_clauses.append(new_clause)
+            
+            # Construct a corresponding SetFromMap invariant.
+            rel = N.get_setfrommap_name(map, mask)
+            sfm = SetFromMapInvariant(rel, map, mask)
+            self.sfm_invs.add(sfm)
+        
+        return L.Name(var)
+
+
+class AggrMapCompRewriter(S.QueryRewriter):
+    
+    """Rewrite comprehension queries so that map lookups from aggregates
+    are flattened into SetFromMap clauses. Return a pair of the new tree
+    and a set of SetFromMap invariants that need to be transformed.
+    """
+    
+    def process(self, tree):
+        self.sfm_invs = OrderedSet()
+        tree = super().process(tree)
+        return tree, self.sfm_invs
+    
+    def rewrite(self, symbol, name, expr):
+        if isinstance(expr, L.Comp):
+            rewriter = AggrMapReplacer(self.symtab.fresh_names.vars)
+            expr = L.rewrite_comp(expr, rewriter.process, recursive=True)
+            self.sfm_invs.update(rewriter.sfm_invs)
+            return expr
+
+
+def transform_comps_with_maps(tree, symtab):
+    """Rewrite all comprehensions containing map lookups, and
+    incrementalize the corresponding SetFromMap invariants.
+    """
+    tree, sfm_invs = AggrMapCompRewriter.run(tree, symtab)
+    for sfm in sfm_invs:
+        tree = transform_setfrommap(tree, symtab, sfm)
+    return tree
---
+++ incoq/tests/mars/aggr/test_comp.py
@@ -0,0 +1,93 @@
+"""Unit tests for comp.py."""
+
+
+import unittest
+
+from incoq.mars.incast import L
+from incoq.mars.symbol import S, N
+from incoq.mars.auxmap import SetFromMapInvariant
+from incoq.mars.aggr.comp import *
+
+
+class CompCase(unittest.TestCase):
+    
+    def test_aggrmapreplacer(self):
+        replacer = AggrMapReplacer(N.fresh_name_generator())
+        
+        result = replacer.process(L.Parser.pe('M[(i,)] + N[(j, k)]'))
+        exp_result = (
+            L.Parser.pe('_v1 + _v2'),
+            [L.SetFromMapMember(['i', '_v1'], 'M', L.mask('bu')),
+             L.SetFromMapMember(['j', 'k', '_v2'], 'N', L.mask('bbu'))],
+        )
+        self.assertEqual(result, exp_result)
+        
+        result = replacer.process(L.Parser.pe('M[(i,)] + O[(k,)]'))
+        exp_result = (
+            L.Parser.pe('_v1 + _v3'),
+            [L.SetFromMapMember(['k', '_v3'], 'O', L.mask('bu'))],
+        )
+        self.assertEqual(result, exp_result)
+        
+        exp_sfm_invs = [
+            SetFromMapInvariant('SM', 'M', L.mask('bu')),
+            SetFromMapInvariant('SN', 'N', L.mask('bbu')),
+            SetFromMapInvariant('SO', 'O', L.mask('bu')),
+        ]
+        self.assertSequenceEqual(list(replacer.sfm_invs), exp_sfm_invs)
+    
+    def test_aggrmapcomprewriter(self):
+        symtab = S.SymbolTable()
+        comp = L.Parser.pe('{x for (x,) in REL(R) if M[(x,)] > 5}')
+        symtab.define_query('Q', node=comp)
+        tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q', {x for (x,) in REL(R) if M[(x,)] > 5}))
+            ''')
+        tree, sfm_invs = AggrMapCompRewriter.run(tree, symtab)
+        exp_tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q', {x for (x,) in REL(R)
+                                    for (x, _v1) in SETFROMMAP(M, 'bu')
+                                    if (_v1 > 5)}))
+            ''')
+        exp_sfm_invs = [
+            SetFromMapInvariant('SM', 'M', L.mask('bu')),
+        ]
+        self.assertEqual(tree, exp_tree)
+        self.assertSequenceEqual(list(sfm_invs), exp_sfm_invs)
+    
+    def test_transform_comps_with_maps(self):
+        symtab = S.SymbolTable()
+        comp = L.Parser.pe('{x for (x,) in REL(R) if M[(x,)] > 5}')
+        symtab.define_map('M')
+        symtab.define_query('Q', node=comp)
+        tree = L.Parser.p('''
+            def main():
+                M.mapassign(k, v)
+                print(QUERY('Q', {x for (x,) in REL(R) if M[(x,)] > 5}))
+            ''')
+        tree = transform_comps_with_maps(tree, symtab)
+        exp_tree = L.Parser.p('''
+            def _maint_SM_for_M_assign(_key, _val):
+                (_key_v1,) = _key
+                _v2_elem = (_key_v1, _val)
+                SM.reladd(_v2_elem)
+            
+            def _maint_SM_for_M_delete(_key):
+                _val = M[_key]
+                (_key_v1,) = _key
+                _v3_elem = (_key_v1, _val)
+                SM.relremove(_v3_elem)
+            
+            def main():
+                M.mapassign(k, v)
+                _maint_SM_for_M_assign(k, v)
+                print(QUERY('Q', {x for (x,) in REL(R)
+                    for (x, _v1) in SETFROMMAP(M, 'bu') if (_v1 > 5)}))
+            ''')
+        self.assertEqual(tree, exp_tree)
+
+
+if __name__ == '__main__':
+    unittest.main()
