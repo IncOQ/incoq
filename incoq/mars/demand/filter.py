7b7f132bd69f4da3e15eb1cb587ec20c5caf2653
Jon Brandvein <jon.brandvein@gmail.com>
2016-02-19 14:04:31 -0500

    Refactor into StructureGenerator

--- incoq/mars/demand/filter.py
+++ incoq/mars/demand/filter.py
@@ -2,7 +2,9 @@
 
 
 __all__ = [
-    'generate_structures',
+    'Filter',
+    'Tag',
+    'StructureGenerator',
 ]
 
 
@@ -39,100 +41,145 @@ class Tag(Struct):
     """Clause that this tag projects."""
 
 
-def generate_structures(clausetools, comp, query_name):
-    """Return a list of tag and filter structures for the given
-    comprehension, in dependency order.
-    """
-    assert isinstance(comp, L.Comp)
-    ct = clausetools
+class StructureGenerator:
     
-    structures = []
-    filters = []
-    filters_by_rel = defaultdict(lambda: [])
-    filters_by_pred = defaultdict(lambda: [])
-    tags = []
-    tags_by_var = defaultdict(lambda: [])
-    tags_by_i = defaultdict(lambda: [])
+    def __init__(self, clausetools, comp, query_name):
+        self.clausetools = clausetools
+        assert isinstance(comp, L.Comp)
+        self.comp = comp
+        self.query_name = query_name
+        
+        # All tags and filters, in definition order (i.e., left-to-right
+        # with respect to the original query comprehension).
+        self.structs = []
+        
+        self.init_indices()
     
-    def add_filter(filter):
-        structures.append(filter)
-        filters.append(filter)
-        rel = ct.rhs_rel(filter.clause)
-        filters_by_rel[rel].append(filter)
-        for p in filter.preds:
-            filters_by_pred[p].append(filter)
+    def init_indices(self):
+        """Initialize/reset indices over structures."""
+        self.filters = []
+        self.filters_by_rel = defaultdict(lambda: [])
+        self.filters_by_pred = defaultdict(lambda: [])
+        self.tags = []
+        self.tags_by_var = defaultdict(lambda: [])
+        self.tags_by_i = defaultdict(lambda: [])
     
-    def add_tag(tag):
-        structures.append(tag)
-        tags.append(tag)
-        tags_by_var[tag.tag_var].append(tag)
-        tags_by_i[tag.i].append(tag)
+    def maint_indices_for_add(self, struct):
+        """Maintain all indices when a new tag or filter structure
+        is appended.
+        """
+        ct = self.clausetools
+        
+        if isinstance(struct, Filter):
+            filter = struct
+            self.filters.append(filter)
+            rel = ct.rhs_rel(filter.clause)
+            self.filters_by_rel[rel].append(filter)
+            for p in filter.preds:
+                self.filters_by_pred[p].append(filter)
+        
+        elif isinstance(struct, Tag):
+            tag = struct
+            self.tags.append(tag)
+            self.tags_by_var[tag.tag_var].append(tag)
+            self.tags_by_i[tag.i].append(tag)
+        
+        else:
+            assert()
     
-    def get_preds(i, in_vars):
+    def add_struct(self, struct):
+        """Add a new structure and maintain indices."""
+        self.structs.append(struct)
+        self.maint_indices_for_add(struct)
+    
+    def recompute_indices(self):
+        """Recompute all indices for arbitrary changes to structures."""
+        self.init_indices()
+        for struct in self.structs:
+            self.maint_indices_for_add(struct)
+    
+    def get_filter_name(self, rel, n):
+        return N.get_filter_name(self.query_name, rel, n)
+    
+    def get_tag_name(self, var, n):
+        return N.get_tag_name(self.query_name, var, n)
+    
+    def get_preds(self, i, in_vars):
         """Return the names of all tags defined for clauses lower than
         clause i, over any of the variables listed in vars.
         """
         result = []
         for v in in_vars:
-            for t in tags_by_var.get(v, []):
+            for t in self.tags_by_var[v]:
                 if not t.i < i:
                     continue
                 result.append(t.name)
         return result
     
-    def change_rhs(cl, rel):
+    def change_rhs(self, cl, rel):
         """Generate a new clause whose RHS rel is as given. This will
         turn object clauses into RelMember clauses.
         """
-        renamer = lambda x: rel
-        return ct.rename_rhs_rel(cl, renamer)
+        return self.clausetools.rename_rhs_rel(cl, lambda x: rel)
     
-    # Generate for each clause.
-    for i, cl in enumerate(comp.clauses):
-        vars = ct.lhs_vars(cl)
-        in_vars = ct.uncon_lhs_vars(cl)
-        out_vars = ct.con_lhs_vars(cl)
-        rel = ct.rhs_rel(cl)
-        if rel is None:
-            raise L.TransformationError('Cannot generate tags and filter '
-                                        'for clause: {}'.format(cl))
+    def make_structs(self):
+        """Populate the structures based on the comprehension."""
+        assert len(self.structs) == 0
+        ct = self.clausetools
         
-        # Generate a filter for this clause.
-        if i != 0:
-            n = len(filters_by_rel[rel]) + 1
-            name = N.get_filter_name(query_name, rel, n)
-            preds = get_preds(i, in_vars)
-            filter = Filter(i, name, cl, preds)
-            add_filter(filter)
-            filtered_cl = change_rhs(cl, name)
-        else:
-            # First clause acts as its own filter; no new structure.
-            filtered_cl = cl
+        # Generate for each clause.
+        for i, cl in enumerate(self.comp.clauses):
+            vars = ct.lhs_vars(cl)
+            in_vars = ct.uncon_lhs_vars(cl)
+            out_vars = ct.con_lhs_vars(cl)
+            rel = ct.rhs_rel(cl)
+            if rel is None:
+                raise L.TransformationError('Cannot generate tags and filter '
+                                            'for clause: {}'.format(cl))
+            
+            # Generate a filter for this clause.
+            if i != 0:
+                n = len(self.filters_by_rel[rel]) + 1
+                name = self.get_filter_name(rel, n)
+                preds = self.get_preds(i, in_vars)
+                filter = Filter(i, name, cl, preds)
+                self.add_struct(filter)
+                filtered_cl = self.change_rhs(cl, name)
+            else:
+                # First clause acts as its own filter; no new structure.
+                filtered_cl = cl
+            
+            # Generate a tag for each variable on the LHS.
+            for var in out_vars:
+                n = len(self.tags_by_var[var]) + 1
+                name = self.get_tag_name(var, n)
+                tag = Tag(i, name, var, filtered_cl)
+                self.add_struct(tag)
+    
+    def simplify_names(self):
+        """Remove numbers from structure names, when not ambiguous."""
+        ct = self.clausetools
         
-        # Generate a tag for each variable on the LHS.
-        for var in out_vars:
-            n = len(tags_by_var[var]) + 1
-            name = N.get_tag_name(query_name, var, n)
-            tag = Tag(i, name, var, filtered_cl)
-            add_tag(tag)
-    
-    # Remove numbers from names when not ambiguous.
-    for filter in filters:
-        rel = ct.rhs_rel(filter.clause)
-        if len(filters_by_rel[rel]) == 1:
-            name = N.get_filter_name(query_name, rel, None)
-            filter.name = name
-            # Update linked tags.
-            for tag in tags_by_i[filter.i]:
-                tag.clause = change_rhs(tag.clause, name)
-    for tag in tags:
-        if len(tags_by_var[tag.tag_var]) == 1:
-            old_name = tag.name
-            name = N.get_tag_name(query_name, tag.tag_var, None)
-            tag.name = name
-            # Update linked filters.
-            for filter in filters_by_pred[old_name]:
-                filter.preds = [name if t == old_name else t
-                                for t in filter.preds]
-    
-    return structures
+        # Not that indices will temporarily become stale while
+        # we're changing names around.
+        
+        for filter in self.filters:
+            rel = ct.rhs_rel(filter.clause)
+            if len(self.filters_by_rel[rel]) == 1:
+                name = self.get_filter_name(rel, None)
+                filter.name = name
+                # Update linked tags.
+                for tag in self.tags_by_i[filter.i]:
+                    tag.clause = self.change_rhs(tag.clause, name)
+        
+        for tag in self.tags:
+            if len(self.tags_by_var[tag.tag_var]) == 1:
+                old_name = tag.name
+                name = self.get_tag_name(tag.tag_var, None)
+                tag.name = name
+                # Update linked filters.
+                for filter in self.filters_by_pred[old_name]:
+                    filter.preds = [name if t == old_name else t
+                                    for t in filter.preds]
+        
+        self.recompute_indices()
--- incoq/tests/mars/demand/test_filter.py
+++ incoq/tests/mars/demand/test_filter.py
@@ -7,7 +7,6 @@ from incoq.mars.incast import L
 from incoq.mars.comp import CoreClauseVisitor
 from incoq.mars.obj import ObjClauseVisitor
 from incoq.mars.demand.filter import *
-from incoq.mars.demand.filter import Tag, Filter
 
 
 class ClauseVisitor(CoreClauseVisitor, ObjClauseVisitor):
@@ -19,13 +18,38 @@ class FilterCase(unittest.TestCase):
     def setUp(self):
         self.visitor = ClauseVisitor()
     
-    def test_generate_structures(self):
+    def test_make_structs(self):
         comp = L.Parser.pe('''
             {(o_f,) for (s, t) in REL(U) for (s, o) in M()
                     for (t, o) in M () for (o, o_f) in F(f)}
             ''')
-        structures = generate_structures(self.visitor, comp, 'Q')
-        exp_structures = [
+        generator = StructureGenerator(self.visitor, comp, 'Q')
+        
+        generator.make_structs()
+        exp_structs = [
+            Tag(0, 'Q_T_s_1', 's', L.RelMember(['s', 't'], 'U')),
+            Tag(0, 'Q_T_t_1', 't', L.RelMember(['s', 't'], 'U')),
+            Filter(1, 'Q_d_M_1', L.MMember('s', 'o'), ['Q_T_s_1']),
+            Tag(1, 'Q_T_o_1', 'o', L.RelMember(['s', 'o'], 'Q_d_M_1')),
+            Filter(2, 'Q_d_M_2', L.MMember('t', 'o'), ['Q_T_t_1']),
+            Tag(2, 'Q_T_o_2', 'o', L.RelMember(['t', 'o'], 'Q_d_M_2')),
+            Filter(3, 'Q_d_F_f_1', L.FMember('o', 'o_f', 'f'),
+                   ['Q_T_o_1', 'Q_T_o_2']),
+            Tag(3, 'Q_T_o_f_1', 'o_f',
+                L.RelMember(['o', 'o_f'], 'Q_d_F_f_1')),
+        ]
+        self.assertEqual(generator.structs, exp_structs)
+    
+    def test_simplify_names(self):
+        comp = L.Parser.pe('''
+            {(o_f,) for (s, t) in REL(U) for (s, o) in M()
+                    for (t, o) in M () for (o, o_f) in F(f)}
+            ''')
+        generator = StructureGenerator(self.visitor, comp, 'Q')
+        generator.make_structs()
+        
+        generator.simplify_names()
+        exp_structs = [
             Tag(0, 'Q_T_s', 's', L.RelMember(['s', 't'], 'U')),
             Tag(0, 'Q_T_t', 't', L.RelMember(['s', 't'], 'U')),
             Filter(1, 'Q_d_M_1', L.MMember('s', 'o'), ['Q_T_s']),
@@ -36,7 +60,7 @@ class FilterCase(unittest.TestCase):
                    ['Q_T_o_1', 'Q_T_o_2']),
             Tag(3, 'Q_T_o_f', 'o_f', L.RelMember(['o', 'o_f'], 'Q_d_F_f')),
         ]
-        self.assertEqual(structures, exp_structures)
+        self.assertEqual(generator.structs, exp_structs)
 
 
 if __name__ == '__main__':
