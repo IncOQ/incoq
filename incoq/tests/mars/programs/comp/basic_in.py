0df4e08226abc2a11e7638d0cc8387cfb12ea8c7
Jon Brandvein <jon.brandvein@gmail.com>
2016-03-08 12:59:19 -0500

    Fix formatting

--- incoq/tests/mars/programs/comp/basic_in.py
+++ incoq/tests/mars/programs/comp/basic_in.py
@@ -11,8 +11,10 @@ S = Set()
 def main():
     for x, y in [(1, 2), (1, 3), (2, 3), (2, 4)]:
         S.add((x, y))
-    print(sorted(QUERY('Q', {(c,) for (a, b) in S for (b2, c) in
-                                  S if b == b2})))
+    print(sorted(QUERY('Q', {(c,) for (a, b) in S for (b2, c) in S
+                                  if b == b2})))
+    
+    # Test clear update.
     S.clear()
     print(sorted(QUERY('Q', {(c,) for (a, b) in S for (b2, c) in S
                                   if b == b2})))
