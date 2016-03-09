52b9819a19df161da45e3992faedfbc815271867
Jon Brandvein <jon.brandvein@gmail.com>
2016-03-08 21:41:01 -0500

    Move relationalize_comp_queries()

--- incoq/mars/transform/relation_rewritings.py
+++ incoq/mars/transform/relation_rewritings.py
@@ -7,10 +7,12 @@ __all__ = [
     'specialize_rels_and_maps',
     'unspecialize_rels_and_maps',
     'expand_clear',
+    'relationalize_comp_queries',
 ]
 
 
 from incoq.mars.incast import L
+from incoq.mars.type import T
 from incoq.mars.symbol import S
 
 
@@ -250,4 +252,83 @@ def expand_clear(tree, symtab):
     MapClear nodes) into elementary update operations.
     """
     tree = ClearExpander.run(tree, symtab.fresh_names.vars)
+    return tree
+
+
+def rewrite_with_unwraps(tree, symtab):
+    """For every comprehension query with a non-tuple result expression,
+    rewrite the result expression to be a singleton tuple and wrap its
+    occurrence in an Unwrap node.
+    """
+    affected_queries = set()
+    
+    class UnwrapRewriter(S.QueryRewriter):
+        def visit_Query(self, node):
+            node = super().visit_Query(node)
+            # Add an Unwrap node.
+            if node.name in affected_queries:
+                node = L.Unwrap(node)
+            return node
+        
+        def rewrite(self, symbol, name, expr):
+            if not isinstance(expr, L.Comp):
+                return
+            if symbol.impl is S.Normal:
+                return
+            comp = expr
+            
+            # No effect if it's already a tuple.
+            if isinstance(comp.resexp, L.Tuple):
+                return
+            affected_queries.add(name)
+            
+            comp = comp._replace(resexp=L.Tuple([comp.resexp]))
+            t = symbol.type
+            t = t.join(T.Set(T.Bottom))
+            assert t.issmaller(T.Set(T.Top))
+            symbol.type = T.Set(T.Tuple([t.elt]))
+            return comp
+    
+    tree = UnwrapRewriter.run(tree, symtab)
+    return tree
+
+
+def rewrite_with_wraps(tree, symtab):
+    """For every Member node in a comprehension query, if the LHS is a
+    single variable, rewrite it as a VarsMember with the RHS in a Wrap
+    node.
+    """
+    def process(expr):
+        if not (isinstance(expr, L.Member) and
+                isinstance(expr.target, L.Name)):
+            return expr, [], []
+        
+        if isinstance(expr.iter, L.Unwrap):
+            rhs = expr.iter.value
+        else:
+            rhs = L.Wrap(expr.iter)
+        cl = L.VarsMember([expr.target.id], rhs)
+        return cl, [], []
+    
+    class WrapRewriter(S.QueryRewriter):
+        def rewrite(self, symbol, name, expr):
+            if not isinstance(expr, L.Comp):
+                return
+            if symbol.impl is S.Normal:
+                return
+            comp = expr
+            
+            comp = L.rewrite_comp(comp, process)
+            return comp
+    
+    tree = WrapRewriter.run(tree, symtab)
+    return tree
+
+
+def relationalize_comp_queries(tree, symtab):
+    """Rewrite each comprehension query so that it is relational. The
+    query must already be flattened.
+    """
+    tree = rewrite_with_unwraps(tree, symtab)
+    tree = rewrite_with_wraps(tree, symtab)
     return tree
--- incoq/tests/mars/transform/test_relation_rewritings.py
+++ incoq/tests/mars/transform/test_relation_rewritings.py
@@ -4,8 +4,11 @@
 import unittest
 
 from incoq.mars.incast import L
+from incoq.mars.type import T
 from incoq.mars.symbol import S, N
 from incoq.mars.transform.relation_rewritings import *
+from incoq.mars.transform.relation_rewritings import (
+    rewrite_with_unwraps, rewrite_with_wraps)
 
 
 class UpdateRewritingsCase(unittest.TestCase):
@@ -131,6 +134,60 @@ class UpdateRewritingsCase(unittest.TestCase):
                     S.remove(_v1)
                 for _v2 in list(M):
                     del M[_v2]
+            ''')
+        self.assertEqual(tree, exp_tree)
+    
+    def test_rewrite_with_unwraps(self):
+        comp1 = L.Parser.pe('''
+            {2 * x for x in R}
+            ''')
+        comp2 = L.Parser.pe('''
+            {(2 * x,) for x in R}
+            ''')
+        comp3 = L.Parser.pe('''
+            {2 * y for (y,) in VARS(QUERY('Q1', {2 * x for x in R}))
+                   for (y,) in VARS(QUERY('Q2', {(2 * x,) for x in R}))}
+            ''')
+        symtab = S.SymbolTable()
+        symtab.define_query('Q1', node=comp1, type=T.Set(T.Number))
+        symtab.define_query('Q2', node=comp2)
+        symtab.define_query('Q3', node=comp3)
+        tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q3', _COMP))
+            ''', subst={'_COMP': comp3})
+        
+        tree = rewrite_with_unwraps(tree, symtab)
+        exp_tree = L.Parser.p('''
+            def main():
+                print(unwrap(QUERY('Q3',
+                {((2 * y),) for (y,) in VARS(unwrap(QUERY('Q1',
+                                {((2 * x),) for x in R})))
+                            for (y,) in VARS(QUERY('Q2',
+                                {((2 * x),) for x in R}))})))
+            ''')
+        self.assertEqual(tree, exp_tree)
+        t = symtab.get_queries()['Q1'].type
+        exp_t = T.Set(T.Tuple([T.Number]))
+        self.assertEqual(t, exp_t)
+    
+    def test_rewrite_with_wraps(self):
+        comp = L.Parser.pe('''
+            {2 * x for x in R for y in unwrap(R) for (z,) in R}
+            ''')
+        symtab = S.SymbolTable()
+        symtab.define_query('Q', node=comp)
+        tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q', _COMP))
+            ''', subst={'_COMP': comp})
+        
+        tree = rewrite_with_wraps(tree, symtab)
+        exp_tree = L.Parser.p('''
+            def main():
+                print(QUERY('Q', {(2 * x) for (x,) in VARS(wrap(R))
+                                          for (y,) in VARS(R)
+                                          for (z,) in R}))
             ''')
         self.assertEqual(tree, exp_tree)
 
